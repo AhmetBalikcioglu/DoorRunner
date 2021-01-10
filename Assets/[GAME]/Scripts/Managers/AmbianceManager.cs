c10324c8081f50c4953ca966809f7f8352dc14df
AhmetBalikcioglu <69127340+AhmetBalikcioglu@users.noreply.github.com>
2021-01-10 20:31:49 +0300

    +add AmbianceManager, Fixed some bugs

---
+++ Assets/[GAME]/Scripts/Managers/AmbianceManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbianceManager : Singleton<AmbianceManager>
+{
+    [Header("AmbianceRelated")]
+    [SerializeField] private List<AmbianceScriptableBase> _ambiances;
+    public AmbianceScriptableBase chosenAmbiance;
+
+    [Header("MaterialRelated")]
+    [SerializeField] private List<Material> _trackMat;
+    [SerializeField] private Material _doorMat;
+    private bool _trackChange;
+
+    private void OnEnable()
+    {
+        if (Managers.Instance == null)
+            return;
+    }
+
+    private void OnDisable()
+    {
+        if (Managers.Instance == null)
+            return;
+    }
+
+    private void Awake()
+    {
+        ChangeAmbiance();
+    }
+    private void ChangeAmbiance()
+    {
+        int randomAmbiance = Random.Range(0, _ambiances.Count);
+        chosenAmbiance = _ambiances[randomAmbiance];
+        Camera.main.backgroundColor = chosenAmbiance.cameraColor;
+        _doorMat.color = chosenAmbiance.doorColor;
+        _trackMat[0].color = chosenAmbiance.trackColor1;
+        _trackMat[1].color = chosenAmbiance.trackColor2;
+        _trackChange = false;
+    }
+
+    public Material ChangeTrackColor()
+    {
+        _trackChange = !_trackChange;
+        if (_trackChange)
+            return _trackMat[0];
+        else
+            return _trackMat[1];
+
+    }
+}
---
+++ Assets/[GAME]/Scripts/Utilities/AmbianceScriptableBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Ambiance", order = 1)]
+public class AmbianceScriptableBase : ScriptableObject
+{
+    [Header("CameraRelated")]
+    public Color cameraColor;
+
+    [Header("TrackRelated")]
+    public Color trackColor1;
+    public Color trackColor2;
+
+    [Header("DoorRelated")]
+    public Color doorColor;
+
+}

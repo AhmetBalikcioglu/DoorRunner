5af53b6664bfc0c2ab3f7d92b27eed0e21ff1fbb
Ali Kızılcan <kizilcanali@gmail.com>
2021-01-11 18:45:00 +0300

    +add Mask, Flags

---
+++ Assets/[GAME]/Scripts/Utilities/FlagAndNickname.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlagAndNickname : MonoBehaviour
+{ 
+    public List<Sprite> FlagImages;
+    private int _randomFlagSelector;
+
+    private void OnEnable()
+    {
+        if (Managers.Instance == null)
+            return;
+        
+        EventManager.OnSceneLoad.AddListener(SpawnFlag);
+    }
+
+    private void OnDisable()
+    {
+        if (Managers.Instance == null)
+            return;
+        
+        EventManager.OnSceneLoad.RemoveListener(SpawnFlag);
+    }
+
+    void SpawnFlag()
+    {
+        _randomFlagSelector = Random.Range(0, FlagImages.Count);
+        GetComponent<Image>().sprite = FlagImages[_randomFlagSelector];
+        
+    }
+}

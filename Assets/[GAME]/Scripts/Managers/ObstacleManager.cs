ac4c1e01498d3e0b651e916cc86b352a9c5b79c4
AhmetBalikcioglu <69127340+AhmetBalikcioglu@users.noreply.github.com>
2021-01-13 22:55:17 +0300

    +add SwipeOutputs

--- Assets/[GAME]/Scripts/Managers/ObstacleManager.cs
+++ Assets/[GAME]/Scripts/Managers/ObstacleManager.cs
@@ -37,8 +37,9 @@ public class ObstacleManager : Singleton<ObstacleManager>
         int randomDoor = Random.Range(0, _doorPrefabs.Count);
         for (int i = 0; i < _lanes.Count; i++)
         {
-            GameObject currentDoor = Instantiate(_doorPrefabs[randomDoor], _lanes[i].position, Quaternion.identity);
-            currentDoor.transform.position += Vector3.up * _doorYIteration + Vector3.forward * iteration * _trackLength;
+            Vector3 doorPosition = _lanes[i].position + Vector3.up * _doorYIteration + Vector3.forward * iteration * _trackLength;
+            GameObject currentDoor = Instantiate(_doorPrefabs[randomDoor], doorPosition, Quaternion.identity);
+            //currentDoor.transform.position += Vector3.up * _doorYIteration + Vector3.forward * iteration * _trackLength;
             Instantiate(_doorFrame, currentDoor.transform.position + (1.5f * Vector3.down), Quaternion.identity);
             randomDoor = Random.Range(0, _doorPrefabs.Count);
         }
